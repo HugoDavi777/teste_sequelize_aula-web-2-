aaaffc593b3ddbe4b42f1d47da352b3e02638fbb
HugoDavi777 <davibatista685@gmail.com>
2025-08-12 11:01:20 -0300

    Delete src directory

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-const DB_URL = "postgresql://postgres:123@localhost:5432/science";
-
-const database = new Pool({
-  connectionString: DB_URL,
-});
-
-module.exports = database;
--- src/models/discovers.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../orm-sequelize");
-
-const discovers = sequelize.define("discovers", {
-    name: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-});
-
-module.exports = discovers;
